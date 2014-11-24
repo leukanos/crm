cd56c61874c4a578bfb90451a0e685a01a7e3315
Łukasz Ostrowski <kontakt@lostrowski.pl>
2014-11-24 10:59:12 +0100

    add enumerize gem

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder'
 
 gem 'rails_admin'
 
+gem 'enumerize'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/models/contact_attribute.rb
+++ app/models/contact_attribute.rb
@@ -1,7 +1,13 @@
 class ContactAttribute
   include Mongoid::Document
+  extend Enumerize
 
   embedded_in :contactable_attribute, polymorphic: true
+
+  scope :email, -> { where(attribute_type: 'email') }
+  scope :phone, -> { where(:attribute_type.in => ['mobile', 'landline'])}
+
+  enumerize :attribute_type, in: [:email, :mobile, :landline, :other]
 
   field :attribute_type
   field :value
