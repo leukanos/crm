d2f724150841f9356fdd76e0ffd9800db77b58a5
Łukasz Ostrowski <kontakt@lostrowski.pl>
2014-11-28 00:32:20 +0100

    enumerize for address_type

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,13 @@
 class Address
   include Mongoid::Document
   include Mongoid::Timestamps
+  extend Enumerize
 
   belongs_to :addressable, polymorphic: true
 
   validates :address_type, presence: true
+
+  enumerize :address_type, in: [:home, :billing]
 
 
   field :address_type
